4681c8d454054c6eff6f54f0759736e210e5c3dd
arbazkhan971 <arbazkhan971@gmail.com>
2018-09-15 00:42:39 +0530

    mysecondcommit

--- index.php
+++ index.php
@@ -2,10 +2,56 @@
 <html>
 <head>
 <title>Best Deal: The E-commerce store</title>
+<link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet"/>
+<link href="styles/bootstrap.min.css" rel="stylesheet"/>
+<link href="styles/style.css" rel="stylesheet" />
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
 </head>
 <body>
+<div id="top">
+<div class="container">
+
+<div class="col-md-6 offer">
+  <a href="#" class="btn btn-success btn-sm">
+  Welcome:Guest
+</a>
+<a href="#" >
+Shopping Cart Total Prize : $200,Total Items 2
+</a>
+</div>
+<div class="col-md-6">
+  <ul class="menu">
+     <li>
+       <a href="customer_register.php">
+       Register
+       </a>
 
 
+     </li>
+    <li>
+      <a href="checkout.php">
+      My Account
+      </a>
+    </li>
+    <li>
+      <a href="cart.php">
+      Go To Cart
+      </a>
+    </li>
+    <li>
+      <a href="checkout.php">
+      Login
+      </a>
+    </li>
 
+  </ul>
+</div>
+</div>
+
+
+</div>
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.min.js"></script>
 </body>
 </html>
