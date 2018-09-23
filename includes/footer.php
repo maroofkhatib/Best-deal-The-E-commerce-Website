8302f722a888e419182e0799a29e60dad93605be
arbazkhan971 <arbazkhan971@gmail.com>
2018-09-23 22:20:47 +0530

    yesterday commit

--- includes/footer.php
+++ includes/footer.php
@@ -79,23 +79,6 @@ echo "<a href='customer/my_account.php?my_orders'>My Account</a>";
 
 <ul><!-- ul Starts -->
 
-<?php
-
-$get_p_cats = "select * from product_categories";
-
-$run_p_cats = mysqli_query($con,$get_p_cats);
-
-while($row_p_cats = mysqli_fetch_array($run_p_cats)){
-
-$p_cat_id = $row_p_cats['p_cat_id'];
-
-$p_cat_title = $row_p_cats['p_cat_title'];
-
-echo "<li> <a href='shop.php?p_cat=$p_cat_id'> $p_cat_title </a> </li>";
-
-}
-
-?>
 
 </ul><!-- ul Ends -->
 
